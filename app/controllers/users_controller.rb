3b5253233b4d2ae491f1406a84b8fd7da49a508e
yuksalbek97 <yuksal97@gmail.com>
2020-06-16 09:52:37 -0400

    crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all 
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.create(user_params)
+    
+end
+
+  def update
+    @user = .find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+     user = User.find(params[:id])
+     User.d
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :age, :gender, :weight, :height, :email)
+  end
+
+
 end
