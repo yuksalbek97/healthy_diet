b995754e7dfa233e4e09406a1cd90b57dc073f9a
yuksalbek97 <yuksal97@gmail.com>
2020-06-16 17:25:09 -0400

    changes

--- app/controllers/carbs_controller.rb
+++ app/controllers/carbs_controller.rb
@@ -1,2 +1,44 @@
 class CarbsController < ApplicationController
+    
+      def index
+        @carbs = Carb.all
+      end
+    
+      def show
+        @carb = Carb.find(params[:id])
+        @result = @carb.results.to_a
+      end
+
+      def new 
+        @carb = Carb.new
+      end
+
+    
+      def create
+        @carb = Carb.create(carb_params)
+        redirect_to carb_path(@carb.id)
+      end
+    
+      def update
+       @carb = Carb.find(params[:id])
+       @carb.update(carb_params)
+       redirect_to carb_path(@carb)
+      end
+    
+      def edit
+        @carb = Carb.find(params[:id])
+      end
+    
+      def destroy
+        @carb = Carb.find(params[:id])
+        @carb.destroy
+        redirect_to carb_path
+
+      end
+
+      private
+
+      def carb_params
+        params.require(:carb).permit(:name, :whole_grain, :cal_per_gram)
+      end
 end
--- app/controllers/proteins_controller.rb
+++ app/controllers/proteins_controller.rb
@@ -1,2 +1,43 @@
 class ProteinsController < ApplicationController
+
+    def index
+        @proteins = Protein.all
+      end
+    
+      def show
+        @protein = Protein.find(params[:id])
+        @result = @protein.results.to_a
+      end
+    
+      def create
+        @protein= Protein.create(protein_params)
+        redirect_to protein_path(@protein.id)
+      end
+
+      def new
+          @protein = Protein.new
+      end
+    
+      def update
+       @protein= Protein.find(params[:id])
+       @protein.update(protein_params)
+       redirect_to protein_path(@protein)
+      end
+    
+      def edit
+        @protein = Protein.find(params[:id])
+      end
+    
+      def destroy
+        @protein = Protein.find(params[:id])
+        @protein.destroy
+        redirect_to protein_path
+
+      end
+
+      private
+
+      def protein_params
+        params.require(:protein).permit(:name, :vegetarian, :cal_per_gram)
+      end
 end
